4a35c64cd749140930e3794317d7643728d9558b
Michael <myque@live.com>
2017-11-18 15:51:07 -0800

    initial express and sequelze setup

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const logger = require('morgan');
+
+const app = express();
+
+app.use(logger('dev'));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.get('*', (req, res) => res.status(200).send({
+	message: 'Interview scheduler',
+}));
+
+module.exports = app;
