588c263a292d364ae89e3c7c16cc83f6a3862fdb
atibhav21 <atibhav.mittal6@gmail.com>
2018-03-07 18:11:09 -0800

    Updated Events API

--- server/controllers/events.js
+++ server/controllers/events.js
@@ -1,14 +1,15 @@
 const Event = require('../models').Event;
 
-const { Client, Query } = require('pg')
-const client = new Client({
-  user: 'dev',
-  host: 'localhost',
-  database: 'events',
-  password: 'password',
-  port: 5432,
-})
-client.connect()
+function pgFormatDate(date) {
+  /* Via http://stackoverflow.com/questions/3605214/javascript-add-leading-zeroes-to-date */
+  function zeroPad(d) {
+    return ("0" + d).slice(-2)
+  }
+
+  var parsed = new Date(date)
+
+  return [parsed.getUTCFullYear(), zeroPad(parsed.getMonth() + 1), zeroPad(parsed.getDate()), zeroPad(parsed.getHours()), zeroPad(parsed.getMinutes()), zeroPad(parsed.getSeconds())].join(" ");
+}
 
 module.exports = {
   create(req, res) {
@@ -17,9 +18,10 @@ module.exports = {
         title: req.body.title,
       })
       .then(event => {
-        const query_text = 'INSERT INTO \"Events\" VALUES($1, $2, $3::timestamptz, $4::timestamptz);';
-     		const query_values = [1, event.title, Date(), Date()];
+        /*const query_text = 'INSERT INTO \"Events\" VALUES($1, $2, $3::timestamptz, $4::timestamptz);';
+     		const query_values = [1, event.title, pgFormatDate(Date()), pgFormateDate(Date())];
 
+        console.log(Date());
         //console.log("Reached here");
 
      		client.query(query_text, query_values, (err, resp) => {
@@ -29,13 +31,13 @@ module.exports = {
      			else {
      				//client.end();
      				res.status(201).send(resp);
-     			}
+     			}*/
+          res.status(201).send(event);
      		})
    		//res.status(201).send(event)
    			/*.then(event => res.status(201).send(event))
    			.catch(error => res.status(400).send(error));*/
       	//res.status(201).send(event)
-      })
       .catch(error => res.status(400).send(error));
 
 
@@ -43,9 +45,9 @@ module.exports = {
   },
   
   read(req, res) {
-    const query_text = 'SELECT * FROM \"Events\" ORDER BY ID; ';
+    //const query_text = 'SELECT * FROM \"Events\" ORDER BY ID; ';
 
-    client.query(query_text, (err, resp) => {
+    /*client.query(query_text, (err, resp) => {
         if(err) {
           res.status(400).send(err)
         }
@@ -57,11 +59,18 @@ module.exports = {
           });
           res.status(201).send(results);
         }
-    })
+    })*/
+    return Event
+            .findById(req.params.id)
+            .then(event => {
+              res.status(201).send(event);
+            }).catch(error => {
+              res.status(400).send(error);
+            });
   },
   
   delete(req, res) {
-    const query_text = 'DELETE FROM \"Events\" WHERE id=$1';
+    /*const query_text = 'DELETE FROM \"Events\" WHERE id=$1';
     const query_values = [req.params.id]
 
     client.query(query_text, query_values, (err, resp) => {
@@ -72,11 +81,19 @@ module.exports = {
       {
         res.status(201).send(resp)
       }
-    });
+    });*/
+    return Event.findById(req.params.id)
+        .then(event => {
+          event.destroy();
+          console.log("Deleted Successfully");
+          res.status(201).send(event);
+        }).catch(error => {
+          res.status(400).send(error);
+        })
   }, 
 
   update(req, res) {
-    const query_text = 'UPDATE \"Events\" SET title=$1 WHERE id=$3; ';
+    /*const query_text = 'UPDATE \"Events\" SET title=$1 WHERE id=$3; ';
     //const query_values = [req.body.title, Date(), req.params.id];
     const query_values = [req.body.title, Date(), req.params.id];
     //console.log(query_text, query_values);
@@ -89,6 +106,16 @@ module.exports = {
       {
         res.status(201).send(resp);
       }
-    });
+    });*/
+    return Event.findById(req.params.id)
+          .then(event => {
+            event.set('title', req.body.title);
+            event.set('updatedAt', Date());
+            event.save();
+            res.status(201).send(event);
+          })
+          .catch(error=> {
+            res.status(400).send(error);
+          });
   }
 }
