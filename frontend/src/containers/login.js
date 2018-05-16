02a35e882cc70cb867e8da5e29854c94df128b53
Alex Zhao <alexzhao21@gmail.com>
2018-05-16 15:06:21 -0700

    More stuff

--- frontend/src/containers/login.js
+++ frontend/src/containers/login.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
+import { Grid, Row, Col } from 'react-bootstrap';
 
 class Login extends Component {
   render()
   {
     return(
-      <div>
-        <h2>Login</h2>
-      </div>
+      <Grid>
+        <Row className="show-grid">
+          <Col xs={12} md={8}>
+            <h2>Login</h2>
+          </Col>
+          <Col xs={6} md={4}>
+            <h3>Login2</h3>
+          </Col>
+        </Row>
+      </Grid>
     );
   }
 }
